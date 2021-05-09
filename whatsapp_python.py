ed23da4c0b0e754f721e62eb74a5b0dbcad0dabb
AS <77235804+crony2004-2011@users.noreply.github.com>
2021-05-09 16:42:18 +0530

    Send alerts from your web application and android app directly to the target number.

---
+++ whatsapp_python.py
@@ -0,0 +1,7 @@
+import pywhatkit
+import datetime
+from datetime import datetime
+x = datetime.now()
+h = x.hour
+m = x.minute
+pywhatkit.sendwhatmsg('+919650084361','Your house is being robbed by niggers', 22, 42)
